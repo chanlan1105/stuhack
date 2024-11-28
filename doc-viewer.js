0356841a9a9733958c667611f30986ae9baa103c
lucas <lucas.r.chan@gmail.com>
2024-11-27 23:10:47 -0500

    Fixed download buttons appearing twice and wonky behaviour

--- doc-viewer.js
+++ doc-viewer.js
@@ -25,25 +25,29 @@ function downloadDoc(){
 	newWindow.document.getElementsByTagName("body")[0].childNodes[0].style = "";
 }
 
-function addButtons(){
-	button1 = document.createElement("button");
-	button1.classList.add("download-button-1");
+function addButtons() {
+	const button1 = document.createElement("button");
+	button1.classList.add("unlocked-download-button");
 	button1.innerHTML = '<svg aria-hidden="true" focusable="false" data-prefix="fas" class="svg-inline--fa" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M537.6 226.6c4.1-10.7 6.4-22.4 6.4-34.6 0-53-43-96-96-96-19.7 0-38.1 6-53.3 16.2C367 64.2 315.3 32 256 32c-88.4 0-160 71.6-160 160 0 2.7.1 5.4.2 8.1C40.2 219.8 0 273.2 0 336c0 79.5 64.5 144 144 144h368c70.7 0 128-57.3 128-128 0-61.9-44-113.6-102.4-125.4zm-132.9 88.7L299.3 420.7c-6.2 6.2-16.4 6.2-22.6 0L171.3 315.3c-10.1-10.1-2.9-27.3 11.3-27.3H248V176c0-8.8 7.2-16 16-16h48c8.8 0 16 7.2 16 16v112h65.4c14.2 0 21.4 17.2 11.3 27.3z"></path></svg><span class="download-text">Download</span>';
-		
-	let prev_buttons = document.getElementsByClassName("fa-cloud-arrow-down");
-	let i = 0;
-	buttons = [];
-	while(prev_buttons.length > 0){
-		if(prev_buttons[0].parentNode.parentNode.firstChild.classList.contains("download-button-1")){
-			prev_buttons[0].parentNode.remove();
-		}else{
-			buttons.push(button1.cloneNode(true, true));
-			buttons[i].onclick = function() {downloadDoc()};
-			prev_buttons[0].parentNode.parentNode.prepend(buttons[i]);
-			prev_buttons[0].parentNode.remove();
-			i++;
+	
+	// Find download buttons that are paywalled
+	const paywalledBtnIcons = document.getElementsByClassName("fa-cloud-arrow-down");
+
+	for (let i = 0; i < paywalledBtnIcons.length; i++) {
+		const paywaleldBtn = paywalledBtnIcons[i].parentNode.parentNode;
+		if (paywaleldBtn.classList.contains("unlocked-download-button")) {
+			// Already unlocked, skip
+			continue;
 		}
-		prev_buttons = document.getElementsByClassName("fa-cloud-arrow-down");
+
+		// Remove paywalled button and replace with a new one
+		const parent = paywaleldBtn.parentNode;
+		paywaleldBtn.remove();
+
+		const unlockedBtn = button1.cloneNode(true);
+		unlockedBtn.onclick = downloadDoc;
+
+		parent.prepend(unlockedBtn);
 	}
 }
 
